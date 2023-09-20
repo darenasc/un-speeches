2132c4069db931d60c8f0121ad4d355856ff7a64
Diego Arenas <darenasc@gmail.com>
2023-09-20 17:32:39 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="",
+    version="0.0.1",
+    description="",
+    url="https://github.com/darenasc/",
+    author="Diego Arenas",
+    author_email="darenasc@gmail.com",
+    license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+)
