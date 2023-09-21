c687ab99c2b1932b08e4034d7ff581e3671e6c58
Diego Arenas C <darenasc@Diegos-MacBook-Air.local>
2023-09-21 13:30:32 +0200

    Add relative path to files

--- app/app.py
+++ app/app.py
@@ -11,14 +11,14 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-df_speech_url = pd.read_csv("UN Speeches.csv")
+df_speech_url = pd.read_csv("./UN Speeches.csv")
 
 countries = df_speech_url["country"].unique()
 country_option = st.sidebar.selectbox("", sorted(list(countries)))
 
 col1, col2 = st.columns(2)
 with col1:
-    image = Image.open(f"wordclouds/{country_option}_words.png")
+    image = Image.open(f"./wordclouds/{country_option}_words.png")
     st.image(image, caption=f"Wordcloud {country_option}")
 with col2:
     st.video(df_speech_url[df_speech_url["country"] == country_option]["url"].values[0])
