a5e7235a9cd34583d112c8697e5ac2a092895b89
Diego Arenas C <darenasc@Diegos-MacBook-Air.local>
2023-09-21 13:38:32 +0200

    Fix absolute path to files

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@ from PIL import Image
 import pandas as pd
 import streamlit as st
 
+path = Path(__file__).absolute()
+
 st.set_page_config(
     page_title="UNGA 2023 Speech Analysis App",
     # page_icon="🧊",
@@ -11,14 +13,14 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-df_speech_url = pd.read_csv("./UN Speeches.csv")
+df_speech_url = pd.read_csv(path / "UN Speeches.csv")
 
 countries = df_speech_url["country"].unique()
 country_option = st.sidebar.selectbox("", sorted(list(countries)))
 
 col1, col2 = st.columns(2)
 with col1:
-    image = Image.open(f"./wordclouds/{country_option}_words.png")
+    image = Image.open(path / "wordclouds" / f"{country_option}_words.png")
     st.image(image, caption=f"Wordcloud {country_option}")
 with col2:
     st.video(df_speech_url[df_speech_url["country"] == country_option]["url"].values[0])
