c73b56bfe0909cc72e869356a247bb7ee5b33377
Diego Arenas C <darenasc@Diegos-MacBook-Air.local>
2023-09-21 13:11:22 +0200

    Add WiP streamlit app

---
+++ app/app.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from PIL import Image
+
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(
+    page_title="UNGA 2023 Speech Analysis App",
+    # page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Get Help": "https://www.extremelycoolapp.com/help",
+        "Report a bug": "https://www.extremelycoolapp.com/bug",
+        "About": "# This is a header. This is an *extremely* cool app!",
+    },
+)
+
+df_speech_url = pd.read_csv("UN Speeches.csv")
+
+countries = df_speech_url["country"].unique()
+country_option = st.sidebar.selectbox("", sorted(list(countries)))
+
+col1, col2 = st.columns(2)
+with col1:
+    image = Image.open(f"wordclouds/{country_option}_words.png")
+    st.image(image, caption=f"Wordcloud {country_option}")
+with col2:
+    st.video(df_speech_url[df_speech_url["country"] == country_option]["url"].values[0])
